86966faf6988113da839238a50628e77cf254d96
chrishyndman@sdarobotics.org <chrishyndman@sdarobotics.org@fc0c4abe-03ff-11df-81fe-833e88af963c>
2011-01-12 02:14:44 +0000

    rewrote iterativerobot properly, simple tank drive with two joys for now

--- trunk/2011/pwmIterative.cpp
+++ trunk/2011/pwmIterative.cpp
@@ -2,73 +2,33 @@
 
 class ParadoxIterative : public IterativeRobot
 {
-  Jaguar*	jagL1 = new Jaguar(1);
-  Jaguar*	jagL2 = new Jaguar(2);
-  Jaguar*	jagR1 = new Jaguar(3);
-  Jaguar*	jagR2 = new Jaguar(4);
-  Joystick*	joystick = new Joystick(1);
+	RobotDrive myRobot;
+	Joystick stickL;
+	Joystick stickR;
 
-  void ParadoxDrive(const float go_Y, const float go_Z)
-  {
-    float speed_L = (go_Y + go_Z);
-    float speed_R = (go_Y - go_Z);  
+public:
+	ParadoxIterative(void):
+		myRobot(1, 2),
+		stickL(1),
+		stickR(2)
+	{
+		myRobot.SetExpiration(0.1);
+	}
+	
+	void AutonomousContinuous(void)
+	{
+		myRobot.TankDrive(0.0, 0.0);
+	}
 
-    if(speed_L > 1) speed_L = 1;
-    if(speed_R > 1) speed_R = 1;
-    if(speed_L < -1) speed_L = -1;
-    if(speed_R < -1) speed_R = -1;
-    
-    jagL1->Set(speed_L);
-    jagL2->Set(speed_L);
-    jagR1->Set(speed_R);
-    jagR2->Set(speed_R);
-  }
-  
-  void RobotInit()
-  {
-    GetWatchdog().SetExpiration(0.5);
-    GetWatchdog().SetEnabled(false);
-  }
-  
-  void AutonomousInit()
-  {
-    GetWatchdog().SetEnabled(false);
-  }
-  
-  void AutonomousContinuous()
-  {
-    // For now robot just twists left and right at half-speed
-    ParadoxDrive(0.5, 0.5);
-    Wait(2.5);
-    ParadoxDrive(0.5, -0.5);
-    Wait(2.5);
-  }
-  
-  void TeleopInit()
-  {
-    GetWatchdog().SetEnabled(true);
-    ParadoxDrive(0,0);
-  }
-
-  void TeleopContinuous()
-  {
-    ParadoxDrive(joystick->GetY(), joystick->GetZ());
-  }
-  
-  void TeleopPeriodic()
-  {
-    GetWatchdog().Feed();
-  }
-  
-  void DisabledInit()
-  {
-    GetWatchdog().SetEnabled(false);
-  }
-  
-  void DisabledPeriodic()
-  {
-    ParadoxDrive(0,0);
-  }
+	void TeleopInit(void)
+	{
+		myRobot.SetSafetyEnabled(true);
+	}
+	
+	void TeleopContinuous(void)
+	{
+		myRobot.TankDrive(stickL.GetY(), stickR.GetY());
+	}
 };
 
 START_ROBOT_CLASS(ParadoxIterative);
