6df323c266a188b77cb506e28582c4d50cfad7d5
chrishyndman@sdarobotics.org <chrishyndman@sdarobotics.org@fc0c4abe-03ff-11df-81fe-833e88af963c>
2011-01-15 22:24:19 +0000

    still working on line tracking...

--- 2011/LinetrackProto.cpp
+++ 2011/LinetrackProto.cpp
@@ -12,6 +12,7 @@ class RobotDemo : public SimpleRobot
         DigitalInput *left;
         DigitalInput *middle;
         DigitalInput *right;
+        DigitalInput *toggle;
         //Arm         *Arm;
         RobotDrive *myRobot; // robot drive system
         Joystick *stickL; // only joystick
@@ -24,6 +25,7 @@ public:
                 left    = new DigitalInput(1);
                 middle  = new DigitalInput(2);
                 right   = new DigitalInput(3);
+                toggle  = new DigitalInput(8);
                 myRobot = new RobotDrive(1, 2); // these must be initialized in the same order
                 stickL  = new Joystick(1);             // as they are declared above.
                 //stickR (2)
@@ -37,48 +39,74 @@ public:
          */
         void Autonomous(void)
         {
+        		myRobot->SetSafetyEnabled(false);
                 Timer *timer = new Timer();
                 timer->Start();
                 timer->Reset();
+                bool toggleValue = toggle->Get()?1:0;
 
                 while (1)
                 {
                 	bool leftValue = left->Get()?1:0 ;      // read the line tracking sensors
                 	bool middleValue = middle->Get()?1:0 ;
                 	bool rightValue = right->Get()?1:0 ;
-                	int  total = leftValue * 4 + middleValue * 2 + rightValue;
+                	int  total = (leftValue * 4 + middleValue * 2 + rightValue);
                 	float turn;
+                	float speed;
                
                 	switch (total)
                 	{
-                	   		case 0:
-                	   				turn = -1;
+                	   		case 7:
+                	   				turn = 0;
+                	   				speed = 0;
                 	   				break;
-                	   		case 1:
-                    	   			turn = 0.6;
-                    	   			break;
-                	   		case 2:
-                	   		        turn = 0.0;
-                	   		        break;
-                	   		case 3:
-                    	   			turn = -0.5;
-                    	   			break; 
-                	   		case 4:
+                	   		case 6:
+                	   				speed = -0.125;
                     	   			turn = -0.6;
                     	   			break;
                 	   		case 5:
-                	   				turn = 0.0;
+                	   				speed = -0.125;
+                	   		        turn = 0.0;
+                	   		        break;
+                	   		case 4:
+                	   				speed = -0.125;
+                    	   			turn = 0.5;
+                    	   			break; 
+                	   		case 3:
+                	   				speed = -0.125;
+                    	   			turn = 0.6;
+                    	   			break;
+                	   		case 2:
+                	   			if (toggleValue==1)
+                	   			{
+                	   				myRobot->Drive(0.0,0.0);
+                	   				Wait(0.15);
+                	   				speed = -0.12;
+                	   				turn = -0.5;
+                	   			}
+                	   			if (toggleValue==0)
+                	   			{
+                	   				myRobot->Drive(0.0,0.0);
+                	   				Wait(0.15);
+                	   				speed = -0.12;
+                	   				turn = 0.5;
+                	   			}
                 	   				break;
-                	   		case 6:
+                	   		case 1:
+                	   				speed = -0.12;
                 	   				turn = 0.5;
                 	   				break;
-                	   		case 7:
-                	   				turn = -1;
+                	   		default:
+                	   				speed = -0.12;
+                	   				turn = 0.0;
                 	   				break;
+                	   		//case 7:
+                	   				//turn = 0;
+                	   				//break;
                         
                 	}
-                myRobot->Drive(1.0,turn);
-                Wait (1.0);
+                myRobot->Drive(speed,turn);
+                Wait (0.25);
                 }
          }
 
