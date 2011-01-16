e4d63da50c730ea75d4f67cf815c590eaa502a83
chrishyndman@sdarobotics.org <chrishyndman@sdarobotics.org@fc0c4abe-03ff-11df-81fe-833e88af963c>
2011-01-16 23:15:55 +0000

    Chris: wrote my own linetracking from scratch, put in pwmIterative.cpp

--- 2011/pwmIterative.cpp
+++ 2011/pwmIterative.cpp
@@ -6,13 +6,21 @@ class ParadoxIterative : public IterativeRobot
 	Jaguar jagR;
 	Joystick gpad;
 	Gyro gyro;
+	DigitalInput lightL;
+	DigitalInput lightC;
+	DigitalInput lightR;
+	DigitalInput LorR;
 	
 public:
 	ParadoxIterative(void):
 		jagL(1),
 		jagR(2),
 		gpad(1),
-		gyro(1)
+		gyro(1),
+		lightL(1),
+		lightC(2),
+		lightR(3),
+		LorR(8)
 	{
 		SetPeriod(0.01);
 		jagL.SetExpiration(0.1);
@@ -30,7 +38,65 @@ public:
 		jagR.Set(0.0);
 	}
 	
-	void TeleopInit(void)
+	void AutonomousContinuous(void)
+	{
+		int Lsees = (lightL.Get()) ? 0 : 100;
+		int Csees = (lightL.Get()) ? 0 : 010;
+		int Rsees = (lightL.Get()) ? 0 : 001;
+		int lineState = Lsees + Csees + Rsees;
+		
+		switch (lineState)
+		{
+			case 010:
+				jagL.Set(0.4);
+				jagR.Set(0.4);
+				break;
+			case 110:
+				jagL.Set(0.0);
+				jagR.Set(0.4);
+				break;
+			case 100:
+				jagL.Set(-0.4);
+				jagR.Set(0.4);
+				break;
+			case 011:
+				jagL.Set(0.4);
+				jagR.Set(0.0);
+				break;
+			case 001:
+				jagL.Set(0.4);
+				jagR.Set(-0.4);
+				break;
+			case 101:
+				gyro.Reset();
+				GyCorrected = 0.0 - GyDrift;
+				if(LorR.Get()==1)
+				{
+					while(GyCorrected < 18.0)
+					{
+						jagL.Set(0.4);
+						jagR.Set(0.0);
+					}
+				}
+				else
+				{
+					while(GyCorrected > -18.0)
+					{
+						jagL.Set(0.0);
+						jagR.Set(0.4);
+					}
+				}
+				jagL.Set(0.4);
+				jagR.Set(0.4);
+				Wait(0.5);
+			default:
+				jagL.Set(-0.2);
+				jagR.Set(-0.2);
+				break;
+		}
+	}
+	
+	void AutonomousInit(void)
 	{
 		gyro.Reset();
 		GyDrift = 0.0;
@@ -40,7 +106,7 @@ public:
 		gyro.Reset();
 	}
 	
-	void TeleopPeriodic(void)
+	void AutonomousPeriodic(void)
 	{
 		GyCorrected = gyro.GetAngle() - (GetPeriod() * GyDrift);
 	}
