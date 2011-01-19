9e8da0e73097ff388ca4db661664c9ce2f799e4f
gabecemaj@sdarobotics.org <gabox8888@users.noreply.github.com>
2011-01-19 15:16:11 +0000

    arm proto

--- 2011/LinetrackProto.cpp
+++ 2011/LinetrackProto.cpp
@@ -4,11 +4,14 @@
 
 class RobotDemo : public SimpleRobot
 {
-		Gyro		 *gyro;
-		DigitalInput *left;
-		DigitalInput *middle;
-		DigitalInput *right;
+		Gyro		 	*gyro;
+		DigitalInput 	*left;
+		DigitalInput  *middle;
+		DigitalInput   *right;
 		DigitalInput  *toggle;
+		Solenoid 	*solenoid;
+		Solenoid   *solenoid2;
+		Jaguar			 *arm;
 		//LineTracking *myTracker;
         //Arm         *Arm;
         RobotDrive 	*myRobot; // robot drive system
@@ -27,6 +30,9 @@ public:
         		middle 		= new DigitalInput (2);
         		right 		= new DigitalInput (3);
         		toggle		= new DigitalInput (8);
+        		solenoid	= new Solenoid	   (1);
+        		solenoid2	= new Solenoid	   (2);
+        		arm 		= new Jaguar	   (5);
         		//myTracker	= new LineTracking(1,2,3); 
                 myRobot 	= new RobotDrive(1, 2); // these must be initialized in the same order
                 stickL  	= new Joystick(1);             // as they are declared above.
@@ -37,13 +43,16 @@ public:
         	Timer *timer = new Timer();
         	timer->Start();
         	timer->Reset();
+        	float x;
+        	float GyDrift;
+        	float GyCorrected;
         	gyro->Reset();
         	GyDrift = 0.0;
-        	Wait(1.0);
+        	Wait(1.0);	
         	GyDrift = gyro->GetAngle();
         	GyCorrected -= GyDrift;
         	gyro->Reset();
-        	float x;
+        	GyCorrected = gyro->GetAngle() - (0.01 * GyDrift);
         			
         	while(timer->Get() < 10.0)
         	{
@@ -55,9 +64,6 @@ public:
         			int  total = leftValue * 4 + middleValue * 2 + rightValue;
         			float turn;
         			float speed;
-        			float GyDrift;
-        			float GyCorrected;
-        			GyCorrected = gyro->GetAngle() - (0.01 * GyDrift);
         			
         			if (timer->Get() == 3.2)
         			{
@@ -91,11 +97,11 @@ public:
         				}
         				break;
         			case 6:
-        				speed = -0.3;
+        				speed = -0.6;
         				turn = -1.0;
         				break;
         			case 5:
-        				speed = -0.3;
+        				speed = -0.6;
         				turn = 0.0;
         				c=0;
         				break;
@@ -104,16 +110,16 @@ public:
         				turn = 1.0;
         				break; 
         			case 3:
-        				speed = -0.3;
+        				speed = -0.6;
         				turn = 1.0;
         				break;
         			case 1:
-        				speed = -0.3;
+        				speed = -0.6;
         				turn = 1.0;
         				break;
         			default:
         				c++;
-        				speed = -0.3;
+        				speed = -0.6;
         				turn = 0.0;
         				if (c>4) speed = 0;
         				break;		                        
@@ -122,13 +128,13 @@ public:
         			myRobot->Drive(speed,turn);
         	}
         	myRobot->Drive(0.0,0.0);
-                while (GyCorrected=!0.0)
+                /*while (GyCorrected=!0.0)
                 {
                 	if (GyCorrected > 0) myRobot->Drive(0.0,0.3);
                 	if (GyCorrected < 0) myRobot->Drive(0.0,-0.3);
                 	GyCorrected = gyro->GetAngle() - (.01* GyDrift);
                 	
-         		}
+         		}*/
         }
         void OperatorControl(void)
         {
@@ -138,7 +144,24 @@ public:
                         
                         myRobot->TankDrive(stickL->GetTwist(),stickL->GetY()); // drive with arcade style (use right stick)
                         Wait(0.005);                            // wait for a motor update time
-                        
+                        if (stickL->GetRawButton(6) == 1)
+                        {
+                        	solenoid->Set(1);
+                        	solenoid2->Set(0);
+                        }
+                        else
+                        {
+                        	solenoid->Set(0);
+                        	solenoid2->Set(1);
+                        }
+                        while (stickL->GetRawButton(4) == 1)
+                        {
+                        	arm->SetSpeed(.5);
+                        }
+                        while (stickL->GetRawButton(2) == 1)
+                        {
+                        	arm->SetSpeed(-.5);
+                        }
                 }
         }
 };
