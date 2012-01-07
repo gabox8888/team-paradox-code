1d25f0c01bf463da91712308ef888e51c15c46db
gabecemaj@sdarobotics.org <gabecemaj@sdarobotics.org@fc0c4abe-03ff-11df-81fe-833e88af963c>
2012-01-07 14:46:24 +0000

    Dump 2011 workspace2 files.  Add folder for 2012!

--- trunk/2011/Arm.h
+++ trunk/2011/Arm.h
@@ -4,24 +4,43 @@
 
 #include "WPILib.h"
 
-static const float kArmPOT_Min = 2.71f;
-static const float kArmPOT_Max = 4.01f;
+//static const float kArmPOT_Min = 2.53f;
+//static const float kArmPOT_Max = 3.62f;
+static const float kArmPOT_Min = 1.7f;
+static const float kArmPOT_Max = 2.66f;
 
 class ParadoxAnalogChannel : public AnalogChannel
 {
 public:
-	explicit ParadoxAnalogChannel(UINT32 channel) : AnalogChannel(channel) { }
+        explicit ParadoxAnalogChannel(UINT32 channel) : AnalogChannel(channel) { }
 
-	virtual double PIDGet()
-	{
-		return GetVoltage();
-	}
+        virtual double PIDGet()
+        {
+                return GetVoltage();
+        }
 };
 
 
 class Arm : public PIDOutput
 {
 public:
+<<<<<<< .mine
+        Arm(UINT32 armvictor, UINT32 pot, UINT32 solen_close, UINT32 solen_open, UINT32 limit);
+        virtual ~Arm() {}
+        
+        void Set(float);
+        void SetPosition(float pot_pos, float sensitivity);
+        void Hand(bool wantopen);
+        void PIDWrite(float output);
+        ParadoxAnalogChannel* GetPot() const { return POT; }
+        void Dump(DriverStationLCD* ds);
+        void ClearPIDVars();
+        float GetPos();
+        float GetSetPoint();
+
+        UINT32 GetLimitSwitch();
+        
+=======
 	Arm(UINT32 shldrChannel, UINT32 upperhandChannel, UINT32 lowerhandChannel, UINT32 extpistonChannel, UINT32 retpistonChannel);
 	virtual ~Arm() {}
 	
@@ -46,12 +65,21 @@ public:
 	
 	
 	
+>>>>>>> .r75
 public:
-	PIDController* m_pPidController;
-	
+        PIDController* m_pPidController;
+        
 protected:
 
 
+<<<<<<< .mine
+        Victor *armVic;
+        ParadoxAnalogChannel *POT;
+        Solenoid *close;
+        Solenoid *open;
+        DigitalInput *limitswitch;
+        
+=======
 	Victor *shldrVictor;
 #ifdef CAN_ENABLED
 	CANJaguar *upperhandJag;
@@ -64,8 +92,9 @@ protected:
 	Solenoid *retSolenoid;
 	Gyro *gyro;
 	
+>>>>>>> .r75
 private:
-	DISALLOW_COPY_AND_ASSIGN(Arm);
+        DISALLOW_COPY_AND_ASSIGN(Arm);
 };
 
 #endif
--- trunk/2011/MyRobot.cpp
+++ trunk/2011/MyRobot.cpp
@@ -1,11 +1,29 @@
 #include "WPILib.h"
 #include "Arm.h"
 #include "LineTracking.h"
-#include "Recorder.h"
+#include "MATH.h"
+
+float kTower_P = 0.0f;
+float kTower_I = 0.0f;
+float kTower_D = 0.0f;
 
 
 class RobotDemo : public IterativeRobot
 {
+public:
+	
+		enum eAutonomousState
+		{
+			kState_LineTrack_Back,
+			kState_Arm_Floor,
+			kState_LineTrack_UpToTube,
+			kState_Arm_PauseForGrip,
+			kState_Arm_UpToPosition,
+			kState_LineTrack_ToPeg,
+			kState_Arm_Hang,
+		};
+
+		
 		Solenoid	   *MiniOut;
 		Solenoid	    *MiniIn;
 		Compressor 	     *spike;
@@ -16,11 +34,22 @@ class RobotDemo : public IterativeRobot
         Joystick 		*stickL;
         Joystick 		*stickR;
         DriverStationLCD 	*ds;
-        Recorder	  *recorder;
         LineTracking *linetracker;
         
+<<<<<<< .mine
+        float				autotime, teleoptime;
+        int 				AutoTime;
+        eAutonomousState 	iAutoState;
+        float            	autoTimer_1;
+        float            	autoTimer_2;
+        float			 	autoTimer_3;
+        bool             	bLatch;
+        float            	m_fSetPos;
+        
+=======
         float autotime, teleoptime;
 
+>>>>>>> .r75
 public:
         RobotDemo() 
         {
@@ -29,26 +58,53 @@ public:
         		MiniIn		= new Solenoid	   (3);
 //        		sonar		= new Ultrasonic (10,11);        		
         		spike		= new Compressor  (14,1);
+<<<<<<< .mine
+        		ParadoxArm  = new Arm(10,2,5,6,6);
+        		//ARM = shoulder, upper roller, lower roller, extend solenoid, retract solenoid
+        		Left		= new Victor(1);
+        		Right		= new Victor(2);
+                myRobot		= new RobotDrive(Left, Right);
+=======
         		ParadoxArm  = new Arm(10,2,3,5,6);
         		//ARM = shoulder, upper roller, lower roller, extend solenoid, retract solenoid
         		Left		= new Victor(1);
         		Right		= new Victor(2);
                 myRobot		= new RobotDrive(Left, Right);
+>>>>>>> .r75
         		stickL  	= new Joystick(1);
         		stickR  	= new Joystick(2);
                 ds			= DriverStationLCD::GetInstance();
-                recorder	= new Recorder();
                 linetracker	= new LineTracking(1,2,3);
+<<<<<<< .mine
+                AutoTime = 0;
+                iAutoState = kState_LineTrack_Back;
+                autoTimer_1 = 0.0f;
+                autoTimer_2 = 0.0f;
+                autoTimer_3 = 0.0f;
+                bLatch = false;
+                m_fSetPos = 0.0f;
+=======
                 //L			= new DigitalInput(4);
                 //M			= new DigitalInput(5);
                 //R			= new DigitalInput(6);
+>>>>>>> .r75
                 
                 MiniOut->Set(0);
                 MiniIn->Set(1); 
                // sonar->SetAutomaticMode(1);
                 myRobot->SetSafetyEnabled(false);
+<<<<<<< .mine
+                SetPeriod(0.025);
+	        	 
+                FILE* fp = fopen("PID.txt", "rb");
+	        	 fscanf(fp, "%f %f %f", &kTower_P, &kTower_I, &kTower_D);
+	        	 fclose(fp);
+
+	        	 printf("Robot Demo Exit\n");
+=======
                 SetPeriod(0.05);
         		printf("Robot Demo Exit\n");
+>>>>>>> .r75
         }
         
         void CommonProcess(float speed, float turn, float shldr, float twist, bool handsuck, bool handeject, bool armextended, bool linetrack)
@@ -79,13 +135,23 @@ public:
         void AutonomousInit(void)
         {
         	autotime = 0.0;
+<<<<<<< .mine
+        	spike->Start();
+            iAutoState = kState_LineTrack_Back;
+            autoTimer_1 = 1.5f; // line track time
+=======
         	spike->Start();
         	ParadoxArm->sucklock = false;
         	recorder->StartPlayback();
+>>>>>>> .r75
         }
         
         void AutonomousPeriodic(void)
         {
+<<<<<<< .mine
+        	/* THIS HAS BEEN DISABLED TO PRESERVE THE LIFE OF THE MOTOR
+        	linetracker->UpdateTotal();
+=======
         	//declare necessary variables,
         	float speed = 0;
         	float turn = 0;
@@ -101,23 +167,199 @@ public:
         	printf("%f %f %f %f %d %d %d %d\n", speed, turn, shldr, twist, (int)handsuck, (int)handeject, (int)armextended, (int)linetrack);
         	
         	if (!handeject) handsuck = true;
+>>>>>>> .r75
+<<<<<<< .mine
+        	const float dT = GetPeriod();
+        	autoTimer_1 -= dT;
+        	autoTimer_2 -= dT;
+        	autoTimer_3 -= dT;
+        	switch (iAutoState)
+        	{
+        		default:
+        			break;
+        		case kState_LineTrack_Back:
+        			if (autoTimer_1 <= 0.0f)
+        			{
+        				iAutoState = kState_Arm_Floor;
+        				bLatch = false;
+        			}
+        			else myRobot->ArcadeDrive(0.0,-0.5);
+        			break;
+        		case kState_Arm_Floor:
+	            		myRobot->Drive(0.0,0.0);
+	            		static const float kArmDownPos = 1.0f;
+	            		ParadoxArm->SetPosition(kArmDownPos, 0.15f);
+	            		ParadoxArm->Hand(true);
+	            		const float piderror = (ParadoxArm->GetPos())-kArmDownPos;//ParadoxArm->m_pPidController->GetError();
+	            		if (fabs(piderror) < 0.05f )
+	            		{
+	            			iAutoState = kState_LineTrack_UpToTube;
+	                        autoTimer_2 = 1.5f; // line track time
+	            		}
+	            		else if (fabs(piderror) < 0.15f)
+	            		{
+	            			if (!bLatch)
+	            			{
+	            				ParadoxArm->ClearPIDVars();
+	            				bLatch = true;
+	            			}
+	            		}
+        			break;
+        		case kState_LineTrack_UpToTube:
+        			if (autoTimer_2 <= 0.0f)
+        			{
+        				iAutoState = kState_Arm_PauseForGrip;
+        				autoTimer_1 = 2.0f;
+        			}
+        			else myRobot->ArcadeDrive(0.0,0.5);
+        			break;
+        		case kState_Arm_PauseForGrip:        			
+        			if (autoTimer_1 <= 0.0f)
+        			{
+        				iAutoState = kState_Arm_UpToPosition;
+        				autoTimer_3 = 3.0f;
+        			}
+        			else
+        			{
+	        			myRobot->Drive(0.0,0.0);
+	        			ParadoxArm->Hand(false);
+        			}
+        			break;
+        		case kState_Arm_UpToPosition:
+        			myRobot->Drive(0.0,0.0);
+        			ParadoxArm->Hand(false);
+        			static const float kStopAt = 0.52f;
+        			ParadoxArm->SetPosition(kStopAt,1);
+        			static float piderror2 =(ParadoxArm->GetPos())-kStopAt;
+        			if (autoTimer_3 <= 0.0f)
+        			{
+        				iAutoState = kState_LineTrack_ToPeg;
+        				autoTimer_1 = 3.9f;
+        			}
+        			break;
+        		case kState_LineTrack_ToPeg:
+        			if (autoTimer_1 <= 0.0f)
+        			{
+        				iAutoState = kState_Arm_Hang;
+        			}
+        			else myRobot->Drive(linetracker->GetSpeed(),linetracker->GetTurn());
+        			break;
+        		case kState_Arm_Hang:
+        			myRobot->Drive(0.0,0.0);
+        			static const float kStopAt2 = 0.56f;
+        			ParadoxArm->SetPosition(kStopAt2,1);
+        			ParadoxArm->Hand(true);
+        		//	static float piderror2 =(ParadoxArm->GetPos())-kStopAt;
+        			break;
+        	}
+        	autotime += dT;
+        	*/
+			/*if(autotime==0.0) myRobot->Drive(-linetracker->GetSpeed(),-linetracker->GetTurn());
+        	if(autotime==2){
+        		myRobot->Drive(0.0,0.0);
+        		ParadoxArm->SetPosition(3.62,1);
+        	}
+        	if((autotime > 3)&&(autotime < 102)&&(fabs(ParadoxArm->m_pPidController->GetError()>=0))){
+        		myRobot->Drive(linetracker->GetSpeed(),linetracker->GetTurn());
+        		if (autotime<100)autotime=100;
+        	}
+        	if(autotime==102){
+        		myRobot->Drive(0.0,0.0);
+        		ParadoxArm->Hand(true);
+        	}
+        	if(autotime==103){
+        		ParadoxArm->SetPosition(3.163632,1);
+        	}
+        	if((autotime > 104)&&(autotime < 205)&&(fabs(ParadoxArm->m_pPidController->GetError()>=0))){
+        		myRobot->Drive(linetracker->GetSpeed(),linetracker->GetTurn());
+        		if (autotime<200)autotime=200;
+        	}
+        	if(autotime==206){
+        		myRobot->Drive(0.0,0.0);
+        		ParadoxArm->SetPosition(1.0,1);
+        		ParadoxArm->Hand(false);
+        	}*/
+        	ds->Clear(); 
+        	ds->Printf(DriverStationLCD::kUser_Line6, 1, "teleoptime %f", teleoptime);
+        	ds->Printf(DriverStationLCD::kUser_Line3, 1, "P: %.2f I: %.2f D: %.2f", kTower_P, kTower_I, kTower_D);
+        	ds->Printf(DriverStationLCD::kUser_Line5, 1, "Arm Pos %f", ParadoxArm->GetPos());
+        	ds->Printf(DriverStationLCD::kUser_Line4, 1, "setPos: %f", ParadoxArm->GetSetPoint());
+        	//ParadoxArm->Dump(ds);
+        	ds->UpdateLCD();
+=======
         	
         	//and apply them.
         	CommonProcess(speed, turn, shldr, twist, handsuck, handeject, armextended, linetrack);
+>>>>>>> .r75
+<<<<<<< .mine
+        	SendDashboardData();
+=======
         	
         	//timer
         	autotime += GetPeriod();
+>>>>>>> .r75
         }
         
         void TeleopInit(void)
         {
+<<<<<<< .mine
+=======
         	recorder->StopPlayback();
+>>>>>>> .r75
         	spike->Start();
         	teleoptime = 0.0;
         } 
         
         void TeleopPeriodic(void)
+        {	
+        	//timer
+        	teleoptime += GetPeriod();
+        }
+        
+        void TeleopContinuous()
         {
+<<<<<<< .mine
+        	//get control values
+        	linetracker->UpdateTotal();
+        	float speed =-1 * stickL->GetY();
+        	float turn = -1 * stickL->GetZ();
+        	float shldr = 0.75 * stickR->GetY(); //MULTIPLIER FOR MOTOR PRESERVATION
+        	bool armextended = (!stickR->GetTrigger());
+        	bool linetrack = stickL->GetRawButton(6);
+        	
+        	//shoulder dead zone
+        	if (fabs(shldr) < 0.1f) shldr = 0.0f;
+        	
+        	//pid override (! = Greg wants it off)
+        	if (!stickR->GetRawButton(3))
+        	{
+        		ParadoxArm->Set(shldr);
+        		m_fSetPos = ParadoxArm->GetPos();
+        		if (m_fSetPos < 0.0f)
+        			m_fSetPos = 0.0f;
+        		else if (m_fSetPos > 1.0f)
+        			m_fSetPos = 1.0f;
+        	}
+        	else
+        	{
+	        	m_fSetPos += shldr * 0.001f;
+	        	if (m_fSetPos < 0.0f)
+	        		m_fSetPos = 0.0f;
+	        	else if (m_fSetPos > 1.0f)
+	        		m_fSetPos = 1.0f;
+	        	ParadoxArm->SetPosition(m_fSetPos,0.15f);
+        	}
+        	
+        	//pid calibrate
+        	if (stickR->GetRawButton(11))
+        	{
+	        	FILE* fp = fopen("PID.txt", "rb");
+	        	fscanf(fp, "%f %f %f", &kTower_P, &kTower_I, &kTower_D);
+	        	fclose(fp);
+        	}
+
+
+=======
             //declare and set necessary setpoints,
             float speed = stickL->GetY();
             float turn = -1 * stickL->GetRawAxis(6);
@@ -142,24 +384,50 @@ public:
         
         void TeleopContinuous()
         {
+>>>>>>> .r75
             //minibot deployment
             MiniOut->Set(stickL->GetRawButton(2));
             MiniIn->Set(!(stickL->GetRawButton(2)));
             
+<<<<<<< .mine
+            //arm
+            ParadoxArm->Hand(armextended);
+            //ParadoxArm->Set(shldr);
+            
+            //driving
+            if (linetrack==true)myRobot->Drive(linetracker->GetSpeed(),linetracker->GetTurn());
+            else myRobot->ArcadeDrive(turn,speed);
+            
+=======
+>>>>>>> .r75
         	//dashboard data
             ds->Clear(); 
+<<<<<<< .mine
+            ds->Printf(DriverStationLCD::kUser_Line6, 1, "teleoptime %f", teleoptime);
+            ds->Printf(DriverStationLCD::kUser_Line3, 1, "P: %.2f I: %.2f D: %.2f", kTower_P, kTower_I, kTower_D);
+            ds->Printf(DriverStationLCD::kUser_Line5, 1, "line %d", linetracker->total);
+            ds->Printf(DriverStationLCD::kUser_Line4, 1, "setPos: %f", ParadoxArm->GetSetPoint());
+            ds->Printf(DriverStationLCD::kUser_Line2, 1, "Arm Pos %f", ParadoxArm->GetPos());
+        	ParadoxArm->Dump(ds);
+=======
             ds->Printf(DriverStationLCD::kUser_Line1, 1, "uppergetcurrent %f", ParadoxArm->Return());
             ds->Printf(DriverStationLCD::kUser_Line5, 1, "teleoptime %f", teleoptime);
             if (recorder->IsRecording) ds->Printf(DriverStationLCD::kUser_Line6, 1, "~~~~~~~RECORDING~~~~~~~");
+>>>>>>> .r75
         	ds->UpdateLCD();
         	SendDashboardData();
+        }
+        
+
+        void DisabledContinuous()
+        {
+        	 ParadoxArm->ClearPIDVars();
+        	 taskDelay(1);
         }
         
         void DisabledInit(void)
         {
         	myRobot->ArcadeDrive(0.0,0.0);
-        	recorder->StopRecording();
-        	recorder->StopPlayback();
         	spike->Stop();
         }
 
