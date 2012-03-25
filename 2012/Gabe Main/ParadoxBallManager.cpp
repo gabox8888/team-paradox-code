41669a5b681a19bc2e43c34384fc9e3dedd05214
gabecemaj@sdarobotics.org <gabox8888@users.noreply.github.com>
2012-03-25 00:14:25 +0000

    Code clean-up, begin matrix coding

--- 2012/Gabe Main/ParadoxBallManager.cpp
+++ 2012/Gabe Main/ParadoxBallManager.cpp
@@ -2,68 +2,19 @@
 #include "ParadoxBallManager.h"
 #include "math.h"
 
-ParadoxBallManager::ParadoxBallManager(UINT32 sucker, UINT32 store, UINT32 feedball, UINT32 in, UINT32 mid, UINT32 out, UINT32 pout, UINT32 pin)
+ParadoxBallManager::ParadoxBallManager(UINT32 store, UINT32 feedball)
 {
-	Sucker 		= new Relay(sucker);
 	Spine		= new Relay(store);
 	Feed		= new CANJaguar(feedball);
-	LimitIN		= new DigitalInput(in);	
-	LimitMID	= new DigitalInput(mid);
-	LimitOUT	= new DigitalInput(out);
-	PistonOut	= new Solenoid(pout);
-	PistonIn	= new Solenoid(pin);
-	
-	counter = 0;
-}
-void ParadoxBallManager::Intake(bool suck)
-{
-	if ((counter<4)&&(suck))Sucker->Set(Relay::kForward);
-	else Sucker->Set(Relay::kOff);
 }
 
 void ParadoxBallManager::Storage(bool storage)
 {
-	if (storage)Spine->Set(Relay::kForward);
-	else Spine->Set(Relay::kOff);
+	Spine->Set((storage) ?  Relay::kForward : Relay::kOff);
 }
 void ParadoxBallManager::FeedToShoot(bool feed, bool rev)
 {
-	if (LimitMID->Get()==1)Feed->Set(1);
-	else if (feed)Feed->Set(-1);
+	if (feed)Feed->Set(-1);
 	else if (rev)Feed->Set(1);
 	else Feed->Set(0);
-}
-void ParadoxBallManager::ShootOut(bool out)
-{
-	if (out) 
-	{
-		PistonOut->Set(0);
-		PistonIn->Set(1);
-	}
-	else
-	{
-		PistonOut->Set(1);
-		PistonIn->Set(0);
-	}
-}
-float ParadoxBallManager::ToCount(void)
-{
-	if (LimitIN->Get()==1)counter++;
-	if (LimitOUT->Get()==1)counter--;
-	return counter;
-}
-void ParadoxBallManager::Practice(INT32 device, INT32 dir)
-{
-	if (device == 0)
-	{
-		if (dir == -1) Sucker->Set(Relay::kReverse);
-		if (dir ==  0) Sucker->Set(Relay::kOff);
-		if (dir ==  1) Sucker->Set(Relay::kForward);
-	}
-	if (device == 1)
-	{
-		if (dir == -1) Spine->Set(Relay::kReverse);
-		if (dir ==  0) Spine->Set(Relay::kOff);
-		if (dir ==  1) Spine->Set(Relay::kForward);
-	}
 }
--- 2012/Gabe Main/ParadoxTipper.cpp
+++ 2012/Gabe Main/ParadoxTipper.cpp
@@ -6,10 +6,7 @@ ParadoxTipper::ParadoxTipper (UINT32 solo, UINT32 soli, UINT32 asonar)
 {
 	BridgeOUT		= new Solenoid(solo);
 	BridgeIN		= new Solenoid(soli);
-	Sonar			= new AnalogChannel(asonar);
 	
-	
-	float inches	= Sonar->GetVoltage()/0.009766; 
 	//myRoutine= ToTip;
 }
 void ParadoxTipper::Tip(bool go)
@@ -24,14 +21,6 @@ void ParadoxTipper::Tip(bool go)
 }
 void ParadoxTipper::Manual(bool manualtip)
 {
-	if (manualtip==true)
-	{
-		BridgeOUT->Set(0);
-		BridgeIN->Set(1);
-	}
-	else
-	{
-		BridgeOUT->Set(1);
-		BridgeIN->Set(0);
-	}
+	BridgeOUT->Set(!manualtip);
+	BridgeIN->Set(manualtip);
 }
