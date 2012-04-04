13ddc02e7ea6c8c71c87c2faca5b5c256010c48c
chrishyndman@sdarobotics.org <chrishyndman@sdarobotics.org@fc0c4abe-03ff-11df-81fe-833e88af963c>
2012-04-04 05:32:20 +0000

    ScatterPlot STABLE.  Added autonomous two-way toggle.  Driving bindings.  WE NEED TO PLOT PLOT PLOT PLOT PLOT!!!!!!!  Otherwise, here comes Madera!

--- 2012/Gabe Main/ParadoxScatterPlot.cpp
+++ 2012/Gabe Main/ParadoxScatterPlot.cpp
@@ -1,4 +1,5 @@
 #include "ParadoxScatterPlot.h"
+#include "WPILib.h"
 
 ParadoxScatterPlot::ParadoxScatterPlot()
 {
@@ -32,7 +33,7 @@ void ParadoxScatterPlot::RebuildCache()
 		l++;
 	}
 	
-	idxmax = l - 1;
+	idxmax = l;
 	
 	in_limbo = false;
 	fclose(tabf);
@@ -42,16 +43,39 @@ int ParadoxScatterPlot::PointSlope(int x)
 {
 	if (!in_limbo)
 	{
-		int a = -1;
-		int b = 9999;
+		printf("PointSlope %d\n", x);
+		printf("idxmax : %d\n", idxmax);
+		int ax = 0;
+		int ay = 0;
+		int bx = 9999;
+		int by = 0;
 		int ret = -1;
-		for (int i = 0; i != idxmax; i++)
+		for (int i = 0; i < idxmax; i++)
 		{
-			if (x == xcache[i]) ret = ycache[i];
-			if ((x > xcache[i]) && (xcache[i] > xcache[a])) a = i;
-			if ((x < xcache[i]) && (xcache[i] < xcache[b])) b = i;
+			printf("i : %d\n", i);
+			printf("x %d, xc %d\n", x, xcache[i]);
+			printf("ax %d, ay %d\n", ax, ay);
+			printf("bx %d, by %d\n", bx, by);
+			if (x == xcache[i])
+			{
+				ret = ycache[i];
+				printf("Case EQ : %d\n", ret);
+			}
+			if ((x > xcache[i]) && (xcache[i] > ax))
+			{
+				ax = xcache[i];
+				ay = ycache[i];
+				printf("Case A : %d\n", ax);
+			}
+			if ((x < xcache[i]) && (xcache[i] < bx))
+			{
+				bx = xcache[i];
+				by = ycache[i];
+				printf("Case B : %d\n", bx);
+			}
 		}
-		if (ret == -1) ret = ((ycache[b] - ycache[a])/(xcache[b] - xcache[a]))*(x - xcache[a]) + ycache[a];
+		if (ret == -1) ret = ((by - ay)/(bx - ax))*(x - ax) + ay;
+		printf("ret : %d\n\n", ret);
 		return ret;
 	}
 	else return 0;
