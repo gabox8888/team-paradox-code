0d96692beb9126bb71f8a33079d8c1933063e59c
chrishyndman@sdarobotics.org <chrishyndman@sdarobotics.org@fc0c4abe-03ff-11df-81fe-833e88af963c>
2012-08-13 23:35:30 +0000

    Stable Code: Calibration routine works for both speed and angle; initial implementation of a stall lock that is not breaking anything;  re-encapsulated appropriate module interfaces.

--- trunk/2012/ParadoxSwerve/Main.cpp
+++ trunk/2012/ParadoxSwerve/Main.cpp
@@ -1,17 +1,22 @@
 #include "ParadoxLib.h"
 
+const float kStallCurrent = 25;
+const float kStallTimeLimit = 0.5;
+
 class ParadoxBot : public IterativeRobot
 {
 	ParadoxModule *Modules[4];
 	Joystick *Joy;
 	DriverStationLCD *ds;
 	ParadoxPersistentArray *CalFile;
-	
+
 	float lowest;
 	int calidx;
 	bool edge[2];
-        bool IsCalibrating;
-	
+	bool IsCalibrating;
+	bool StallLock;
+	float StallTime;
+
 public:
 	ParadoxBot()
 	{
@@ -19,87 +24,109 @@ public:
 		Modules[1] = new ParadoxModule(32, 31, 3, 2); //Blue One
 		Modules[2] = new ParadoxModule(42, 41, 4, 3); //Blue Two
 		Modules[3] = new ParadoxModule(12, 11, 1, 4); //White One
-		
+
 		CalFile = new ParadoxPersistentArray("calibrate.txt", 5);
-		
+
 		for (int i = 0; i < 4; i++)
 		{
-			Modules[i]->TopSpeed = CalFile->Read(0);
-			Modules[i]->Offset = CalFile->Read(i + 1);
+			Modules[i]->SetTopSpeed(CalFile->Read(0));
+			Modules[i]->SetOffset(CalFile->Read(i + 1), false);
 		}
 
 		Joy	= new Joystick(1);
 		ds	= DriverStationLCD::GetInstance();
-		
+
+		SetPeriod(0.05);
 	};
 
 	~ParadoxBot(){}
-	
+
 	void TeleopPeriodic(void)
 	{
 		bool CalKeyCombo = Joy->GetRawButton(1) && Joy->GetRawButton(11) && Joy->GetRawButton(9);
-		
-		if (CalKeyCombo)
+
+		if (StallLock)
 		{
-                        IsCalibrating = true;
-			edge[0] = edge[1];
-			edge[1] = Joy->GetRawButton(2);
-			if (!edge[0] && edge[1]) calidx++;
-			if (calidx > 3) calidx = -1;
-			
-			ds->PrintfLine(DriverStationLCD::kUser_Line1, "##### CALIBRATING #####");
-			for (int i = 0; i < 4; i++) Modules[i]->Calibrate((calidx == i));
-			if (calidx >= 0)
-			{
-				if (Joy->GetRawButton(3)) Modules[calidx]->Offset += 0.5*GetPeriod();
-				if (Joy->GetRawButton(4)) Modules[calidx]->Offset -= 0.5*GetPeriod();
-				
-				ds->PrintfLine(DriverStationLCD::kUser_Line2, "ANGLE ADJUST");
-				ds->PrintfLine(DriverStationLCD::kUser_Line3, "selected wheel %d", calidx);
-				ds->PrintfLine(DriverStationLCD::kUser_Line4, "offset %f", Modules[calidx]->Offset);
-			}
-			else
-			{
-				lowest = 9999;
-				for (int i = 0; i < 4; i++)
-				{
-					float gv = Modules[i]->GetValue(ParadoxModule::kSpeed);
-					if ((gv < lowest) && (gv > 10)) lowest = gv;
-				}
-				for (int i = 0; i < 4; i++) Modules[i]->TopSpeed = lowest;
-				
-				ds->PrintfLine(DriverStationLCD::kUser_Line2, "TOP SPEED");
-				ds->PrintfLine(DriverStationLCD::kUser_Line3, "lowest %.2f", lowest);
-			}
+			ds->PrintfLine(DriverStationLCD::kUser_Line1, "!!!");
+			ds->PrintfLine(DriverStationLCD::kUser_Line2, "!!! STALL CURRENT REACHED");
+			ds->PrintfLine(DriverStationLCD::kUser_Line3, "!!! Press Button 7 To Unlock");
+			ds->PrintfLine(DriverStationLCD::kUser_Line4, "!!!");
+			if (Joy->GetRawButton(7)) StallLock = false;
+			else {for (int i = 0; i < 4; i++) Modules[i]->AllStop();}
 		}
 		else
 		{
-                        if (IsCalibrating)
-                        {
-                                float values[5];
-                                values[0] = lowest;
-                                for (int i = 0; i < 4; i++) values[i + 1] = Modules[i]->Offset;
-                                CalFile->Write(values);
-                                IsCalibrating = false;
-                        }
-			calidx = -1;
-			edge[0] = false;
-			edge[1] = false;
-			
-			float highest = 1;
-			for (int i = 0; i < 4; i++)
+			if (CalKeyCombo)
 			{
-				float sp = Modules[i]->SetPropose(Joy);
-				if (sp > highest) highest = sp;
+				if (!IsCalibrating)
+				{
+					ds->Clear();
+					IsCalibrating = true;
+				}
+				edge[0] = edge[1];
+				edge[1] = Joy->GetRawButton(2);
+				if (!edge[0] && edge[1]) calidx++;
+				if (calidx > 3) calidx = -1;
+	
+				ds->PrintfLine(DriverStationLCD::kUser_Line1, "##### CALIBRATING #####");
+				for (int i = 0; i < 4; i++) Modules[i]->Calibrate((calidx == -1) || (calidx == i));
+				if (calidx >= 0)
+				{
+					Modules[calidx]->SetOffset(Joy->GetZ()*Joy->GetZ()*(Joy->GetZ() < 0 ? -1 : 1)*GetPeriod()*-1, true);
+	
+					ds->PrintfLine(DriverStationLCD::kUser_Line2, "ANGLE ADJUST");
+					ds->PrintfLine(DriverStationLCD::kUser_Line3, "selected wheel %d", calidx);
+					ds->PrintfLine(DriverStationLCD::kUser_Line4, "offset %f", Modules[calidx]->GetOffset());
+				}
+				else
+				{
+					lowest = 9999;
+					for (int i = 0; i < 4; i++)
+					{
+						float gv = Modules[i]->GetValue(ParadoxModule::kSpeed);
+						if ((gv < lowest) && (gv > 10)) lowest = gv;
+					}
+					for (int i = 0; i < 4; i++) Modules[i]->SetTopSpeed(lowest);
+	
+					ds->PrintfLine(DriverStationLCD::kUser_Line2, "TOP SPEED");
+					ds->PrintfLine(DriverStationLCD::kUser_Line3, "lowest %.2f", lowest);
+				}
 			}
-			
-			for (int i = 0; i < 4; i++) Modules[i]->SetCommit(highest);
-			
-			ds->PrintfLine(DriverStationLCD::kUser_Line1, "FRONT (get angle)");
-			ds->PrintfLine(DriverStationLCD::kUser_Line2, "%.2f %.2f",
-                                       Modules[1]->GetValue(ParadoxModule::kPot), Modules[0]->GetValue(ParadoxModule::kPot));
-			ds->PrintfLine(DriverStationLCD::kUser_Line3, "%.2f %.2f",
-                                       Modules[2]->GetValue(ParadoxModule::kPot), Modules[3]->GetValue(ParadoxModule::kPot));
+			else
+			{
+				if (IsCalibrating)
+				{
+					ds->Clear();
+					float values[5];
+					values[0] = lowest;
+					for (int i = 0; i < 4; i++) values[i + 1] = Modules[i]->GetOffset();
+					CalFile->Write(values);
+					IsCalibrating = false;
+				}
+				calidx = -1;
+				edge[0] = false;
+				edge[1] = false;
+	
+				float highest = 1;
+				for (int i = 0; i < 4; i++)
+				{
+					float sp = Modules[i]->SetPropose(Joy);
+					if (sp > highest) highest = sp;
+				}
+	
+				for (int i = 0; i < 4; i++) Modules[i]->SetCommit(highest);
+	
+				ds->PrintfLine(DriverStationLCD::kUser_Line1, "FRONT (get amps)");
+				ds->PrintfLine(DriverStationLCD::kUser_Line2, "%.2f %.2f",
+						Modules[1]->GetValue(ParadoxModule::kAmps), Modules[0]->GetValue(ParadoxModule::kAmps));
+				ds->PrintfLine(DriverStationLCD::kUser_Line3, "%.2f %.2f",
+						Modules[2]->GetValue(ParadoxModule::kAmps), Modules[3]->GetValue(ParadoxModule::kAmps));
+			}
+			bool test_stall = false;
+			for (int i = 0; i < 4; i++) {if (Modules[i]->GetValue(ParadoxModule::kAmps) > kStallCurrent) test_stall = true;}
+			if (test_stall) StallTime += GetPeriod();
+			else StallTime = 0;
+			if (StallTime > kStallTimeLimit) StallLock = true;
 		}
 		ds->UpdateLCD();
 	}
--- trunk/2012/ParadoxSwerve/ParadoxModule.cpp
+++ trunk/2012/ParadoxSwerve/ParadoxModule.cpp
@@ -22,18 +22,18 @@ ParadoxModule::ParadoxModule(UINT32 angle_w,UINT32 speed_w, UINT32 absenc, UINT3
 	Angle	= new CANJaguar(angle_w);
 	Speed	= new CANJaguar(speed_w, CANJaguar::kSpeed);
 	POT		= new ParadoxAnalogChannel(absenc);
-	
+
 	AngPID	= new PIDController(kAngle_P, kAngle_I, kAngle_D, POT, Angle);
 	AngPID->Enable();
 	AngPID->SetInputRange(kAngle_Min,kAngle_Max);
 	AngPID->SetContinuous(true);
-	
+
 	Speed->SetSpeedReference(CANJaguar::kSpeedRef_Encoder);
 	Speed->EnableControl();
 	Speed->SetSafetyEnabled(false);
 	Speed->ConfigEncoderCodesPerRev(kSpeed_CPR);
 	Speed->SetPID(kSpeed_P, kSpeed_I, kSpeed_D);
-	
+
 	Wdir = (-0.25 + (quadrant + 1)*0.5)*kPi;
 	WasCalibrating = false;
 }
@@ -50,11 +50,11 @@ float ParadoxModule::SetPropose(Joystick *joy)
 	float Vdir = -1.0*joy->GetDirectionRadians() + 0.5*kPi;
 	if (Vmag < kDeadZone) Vmag = 0;
 	ParadoxVector *V = new ParadoxVector(Vmag, Vdir);
-	
+
 	float Wmag = -1.0*joy->GetZ();
 	if (fabs(Wmag) < kDeadZone) Wmag = 0;
 	ParadoxVector *W = new ParadoxVector(Wmag, Wdir);
-	
+
 	ParadoxVector *Sum = new ParadoxVector(V, W);
 	spd_proposal = Sum->GetMagnitude();
 	ang_proposal = Sum->GetDirection();
@@ -62,19 +62,19 @@ float ParadoxModule::SetPropose(Joystick *joy)
 	delete V;
 	delete W;
 	delete Sum;
-	
+
 	return spd_proposal;
 }
 
 void ParadoxModule::SetCommit(float max)
 {
 	if (WasCalibrating)
-        {
-                Speed->Set(0);
+	{
+		Speed->Set(0);
 		Speed->ChangeControlMode(CANJaguar::kSpeed);
 		Speed->EnableControl();
 		WasCalibrating = false;
-        }
+	}
 	ang_proposal += Offset;
 	while (ang_proposal > 2*kPi) ang_proposal -= 2*kPi;
 	while (ang_proposal < 0) ang_proposal += 2*kPi;
@@ -82,7 +82,7 @@ void ParadoxModule::SetCommit(float max)
 	Speed->Set((spd_proposal / max)*TopSpeed);
 }
 
-void ParadoxModule::Calibrate(bool ang_mode)
+void ParadoxModule::Calibrate(bool run_speed)
 {
 	if (!WasCalibrating)
 	{
@@ -90,15 +90,32 @@ void ParadoxModule::Calibrate(bool ang_mode)
 		Speed->ChangeControlMode(CANJaguar::kVoltage);
 		Speed->EnableControl();
 		WasCalibrating = true;
-    }
-	Speed->Set(ang_mode ? 4 : kCalibrateVoltage);
-	AngPID->SetSetpoint((5/(2*kPi))*(Wdir += Offset));
+	}
+	Speed->Set(run_speed ? kCalibrateVoltage : 0);
+	AngPID->SetSetpoint((5/(2*kPi))*(Wdir + Offset));
 }
 
 float ParadoxModule::GetValue(ModuleValue mv)
 {
 	if (mv == kSpeed) return Speed->GetSpeed();
 	else if (mv == kPot) return POT->GetVoltage();
+	else if (mv == kAmps) return Speed->GetOutputCurrent();
 	else return 0;
 }
 
+void ParadoxModule::SetTopSpeed(float ts)
+{
+	TopSpeed = ts;
+}
+
+void ParadoxModule::SetOffset(float os, bool add_current)
+{
+	if (add_current) Offset += os;
+	else Offset = os;
+}
+
+void ParadoxModule::AllStop()
+{
+	Speed->Set(0);
+	AngPID->SetSetpoint(POT->GetVoltage());
+}
