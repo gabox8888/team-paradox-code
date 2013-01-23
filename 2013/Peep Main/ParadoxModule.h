5481b504fd3331f26e6249097f67c1269a6ca28a
42catfish@sbcglobal.net <42catfish@sbcglobal.net@fc0c4abe-03ff-11df-81fe-833e88af963c>
2013-01-23 01:23:55 +0000

    Finished the TODO

--- 2013/Peep Main/ParadoxModule.h
+++ 2013/Peep Main/ParadoxModule.h
@@ -1,20 +1,14 @@
 #ifndef PARADOXMODULE_H_
 #define PARADOXMODULE_H_
 
-/* TODO:
-Change naming from spd to speed
-Define what hardware is in a ParadoxModule-> CANJaguar and Encoder
-Define JagChannel-> Port number on the CAN network
-Change naming CalibrateTopSpeed to SetTopSpeed
-*/
-
 /*
  * ParadoxModule.h
  * Copyright (c) Team Paradox 2102 Year: 2013. All rights reserved.
  * Paradox Module.
  * This class serves to create an object in the code that represents a single
- * independently controlled gearbox.
- * 
+ * independently controlled gearbox as well as the Jaguar and Encoder wired to it.
+ * The Encoder is connected directly to the Jaguar. JagChannel is the Jaguar's number
+ * on the CAN bus.
  * Authors: Paradox++
  */
 
@@ -23,21 +17,23 @@ Change naming CalibrateTopSpeed to SetTopSpeed
 
 class ParadoxModule
 {
-	public:			//REMEMBER: Change any of these that can be protected to protected
+public:			
 		ParadoxModule(UINT32 JagChannel);
 		virtual ~ParadoxModule(){};
 		
 		double GetRPM();
-		void SetRPM(float spd);
-		void CalibrateTopSpeed(float spd);
+    void SetRPM(float speed);
+    void SetSpeedVoltage(float speed);
+		void SetTopSpeed(float speed);
 		
 	protected:
 		CANJaguar *JagSpeed;
 		
 		float FltTopSpeed;
 		
-	private:
-        DISALLOW_COPY_AND_ASSIGN(ParadoxModule);
+  private:
+    void InitParadoxModule();
+    DISALLOW_COPY_AND_ASSIGN(ParadoxModule);
 };
 
 #endif
