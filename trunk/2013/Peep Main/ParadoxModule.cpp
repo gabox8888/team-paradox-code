040641f7634ca6d89dfc2b91a6bd09be47b3ebbc
42catfish@sbcglobal.net <42catfish@sbcglobal.net@fc0c4abe-03ff-11df-81fe-833e88af963c>
2013-01-23 01:23:47 +0000

    Finished most of the TODO

--- trunk/2013/Peep Main/ParadoxModule.cpp
+++ trunk/2013/Peep Main/ParadoxModule.cpp
@@ -1,19 +1,19 @@
 /*
-TODO:
-Create a private function (InitParadoxModule) and move all the init Jag code into it. To be called in constructor
-Collapse all the comments. No extra space in comments
-Add function to set the speed in voltage mode. Call it SetSpeedVoltage
+TO DO:
+Finish SetSpeedVoltage. Convert Joystick input into Voltage.
 */
 
 /*
- * ParadoxModule.cpp
- * Copyright (c) Team Paradox 2102 Year: 2013. All rights reserved
+ * ParadoxModule.h
+ * Copyright (c) Team Paradox 2102 Year: 2013. All rights reserved.
  * Paradox Module.
  * This class serves to create an object in the code that represents a single
- * independently controlled gearbox.
- * 
+ * independently controlled gearbox as well as the Jaguar and Encoder wired to it.
+ * The Encoder is connected directly to the Jaguar. JagChannel is the Jaguar's number
+ * on the CAN bus.
  * Authors: Paradox++
  */
+
 
 #include "ParadoxModule.h"
 
@@ -26,19 +26,13 @@ Add function to set the speed in voltage mode. Call it SetSpeedVoltage
 
 ParadoxModule::ParadoxModule(UINT32 channel)
 {
-	JagSpeed = new CANJaguar(channel);
-	
-	JagSpeed->ChangeControlMode(CANJaguar::kSpeed);
-	JagSpeed->SetSpeedReference(CANJaguar::kSpeedRef_Encoder);
-	JagSpeed->EnableControl();
-	JagSpeed->SetSafetyEnabled(false);
-	JagSpeed->ConfigEncoderCodesPerRev(TicksPerRev);
-	JagSpeed->SetPID(0,0,0);
-}
+  JagSpeed = new CANJaguar(channel);
+  
+  InitParadoxModule();
+ }
 
 /**
  * Get the current speed in rotations per minute.
- * 
  * @return The current speed as a double.
  */
 
@@ -49,22 +43,50 @@ double ParadoxModule::GetRPM()
 
 /**
  * Sets the jaguar to a speed expressed in rotations per minute.
- * 
- * @param spd A value between 1 and -1 representing the input
+ * @param speed A value between 1 and -1 representing the input
  */
 
-void ParadoxModule::SetRPM(float spd)
+void ParadoxModule::SetRPM(float speed)
 {
-  JagSpeed->Set(FltTopSpeed * spd); 
+  JagSpeed->Set(FltTopSpeed * speed); 
+}
+
+/**
+ * Sets the jaguar to a speed expressed in voltage and then returns
+ * the control mode to RPM.
+ * @param speed A value between representing the input in voltage
+ */
+
+void ParadoxModule::SetSpeedVoltage(float speed)
+{
+  JagSpeed->ChangeControlMode(CANJaguar::kVoltage);
+
+  JagSpeed->Set(); 
+  JagSpeed->ChangeControlMode(CANJaguar::kSpeed);
 }
 
 /**
  * Determines the top speed and sets it to FltTopSpeed
  */
 
-void ParadoxModule::CalibrateTopSpeed(float spd)
+void ParadoxModule::SetTopSpeed(float speed)
 {
 	JagSpeed->ChangeControlMode(CANJaguar::kPercentVbus);
 	JagSpeed->Set(spd);
 	FltTopSpeed = JagSpeed->GetSpeed();
+  JagSpeed->ChangeControlMode(CANJaguar::kSpeed);
+}
+
+/*
+ * Initialization code for the Jaguar.
+ */
+
+void ParadoxModule::InitParadoxModule()
+{   
+	JagSpeed->ChangeControlMode(CANJaguar::kSpeed);
+	JagSpeed->SetSpeedReference(CANJaguar::kSpeedRef_Encoder);
+	JagSpeed->EnableControl();
+	JagSpeed->SetSafetyEnabled(false);
+	JagSpeed->ConfigEncoderCodesPerRev(TicksPerRev);
+	JagSpeed->SetPID(0,0,0);
 }
