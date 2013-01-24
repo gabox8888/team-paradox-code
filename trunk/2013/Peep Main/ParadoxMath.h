efa47e9ea818a92b303acf438ae56b222c61bdef
42catfish@sbcglobal.net <42catfish@sbcglobal.net@fc0c4abe-03ff-11df-81fe-833e88af963c>
2013-01-24 03:04:01 +0000

    ParadoxMath.h

---
+++ trunk/2013/Peep Main/ParadoxMath.h
@@ -0,0 +1,19 @@
+#ifndef PARADOXMATH_H_
+#define PARADOXMATH_H_
+
+#include "WPILib.h"
+#include "ParadoxMath.h"
+
+static const float kPI = 3.14159265f;
+
+class ParadoxMath
+{
+   public:
+        ParadoxMath();
+        virtual ~ParadoxMath() {}
+        float GetLowest(float array[]);
+       
+   private:
+        DISALLOW_COPY_AND_ASSIGN(ParadoxMath);
+};
+#endif
