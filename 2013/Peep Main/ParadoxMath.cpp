94642fa9e88c8e7aee55dc25f6126c2fab6b4683
meliceiri@gmail.com <meliceiri@gmail.com@fc0c4abe-03ff-11df-81fe-833e88af963c>
2013-01-25 01:03:35 +0000

    created find lowest array value function

--- 2013/Peep Main/ParadoxMath.cpp
+++ 2013/Peep Main/ParadoxMath.cpp
@@ -1,6 +1,7 @@
 #include "WPILib.h" 
 #include "ParadoxMath.h" 
 #include "math.h"
+#define ARRAYSIZE 4
 
 ParadoxMath::ParadoxMath()
 {
@@ -9,8 +10,16 @@ ParadoxMath::ParadoxMath()
 
 float ParadoxMath::GetLowest(float array[])
 {
-
+for(int i=0;i++; i < ARRAYSIZE)
+{
+if(array[i]<array[i+1])
+{
+FltLowValue=array[i];
 }
-
+else
+{
+FltLowValue=array[i+1];
+}
+}
 
 
